43c7b763b0ac25fcdbc73ba1649d98ea83efb1c3
Gabriel Matos <matosgabriel017@gmail.com>
2020-09-03 10:01:48 -0300

    create past  "models  and model ''user" add dummy_users"

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,24 @@
+import '../models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Bob',
+    email: 'bob@example.com.br',
+    profileAvatar:
+        'https://cdn.pixabay.com/photo/2016/11/01/21/11/avatar-1789663_960_720.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Carl',
+    email: 'carl@example.com.br',
+    profileAvatar:
+        'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.pngg',
+  ),
+  '3': const User(
+      id: '3',
+      name: "Rachel",
+      email: 'rachel@example.com.br',
+      profileAvatar:
+          'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'),
+};
